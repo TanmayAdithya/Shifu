af76ac0f7c5ae9511de66e9eec6b7c8e10db66b1
TanmayAdithya <marnitanmayadithya@gmail.com>
2024-05-08 17:18:16 +0530

    prettier x tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <>
-      <h1>Bum</h1>
+      <main className="grid h-screen w-screen place-items-center">
+        <div className="relative h-[400px] w-[400px] border-2 border-solid border-white">
+          <div className="absolute left-0 top-0 h-[40px] w-[40px] cursor-pointer bg-orange-200"></div>
+        </div>
+      </main>
     </>
   );
 }
