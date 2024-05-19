9034e31a33ec68d45a55a0a69096a42f37854972
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-05-19 11:05:18 +0530

    feat: Adjust background change button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,11 +7,31 @@ const Navbar = () => {
 
   const handleClick = (id: number) => {
     setActiveItem(id === activeItem ? null : id);
+
+    // Change background
+    const bgChanger = document.getElementById("background-changer");
+    if (id === 8) {
+      if (bgChanger) {
+        bgChanger.classList.add("bg-slate-200", "hover:bg-slate-200");
+      }
+    } else {
+      bgChanger?.classList.remove("bg-slate-200", "hover:bg-slate-200");
+    }
   };
+
   return (
     <div className="absolute z-40 mb-[3rem] flex items-center">
-      <div className="mr-2  flex h-14 w-14 flex-auto cursor-pointer items-center justify-center rounded-xl border-0 bg-neutral-50 px-1 py-1 shadow-lg transition-colors duration-200 ease-in-out hover:bg-slate-100">
-        <Picture key={8} size={"1.35rem"} color="#262626" />
+      <div className="mr-2 rounded-xl bg-slate-50 px-1 py-1">
+        <li
+          id="background-changer"
+          key={8}
+          className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-xl transition-colors duration-200 ease-in-out hover:bg-slate-100"
+          onClick={() => {
+            handleClick(8);
+          }}
+        >
+          <span>{<Picture size={"1.35rem"} color="#262626" />}</span>
+        </li>
       </div>
       <div className="w-[24rem] items-center justify-center rounded-xl border-0 bg-neutral-50 px-1 py-1 shadow-lg">
         <div className="flex flex-1 justify-between">
@@ -19,7 +39,7 @@ const Navbar = () => {
             const isActive = id === activeItem;
             return (
               <li
-                className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-200 ease-in-out hover:bg-slate-100 ${isActive ? " bg-slate-200" : ""}`}
+                className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-200 ease-in-out hover:bg-slate-100 ${isActive ? " bg-slate-200 hover:bg-slate-200" : ""}`}
                 onClick={() => {
                   handleClick(id);
                 }}
