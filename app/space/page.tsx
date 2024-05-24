ae378d17debe7cc57fb9460e75a7d2a3fc19d4bf
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-05-24 23:39:49 +0530

    feat: Add react-draggable library

--- app/space/page.tsx
+++ app/space/page.tsx
@@ -1,72 +1,13 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import Draggable from "react-draggable";
 
 export default function page() {
-  const boxRef = useRef<HTMLDivElement>(null);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const isClicked = useRef<boolean>(false);
-  const coords = useRef<{
-    startX: number;
-    startY: number;
-    lastX: number;
-    lastY: number;
-  }>({ startX: 0, startY: 0, lastX: 0, lastY: 0 });
-
-  useEffect(() => {
-    if (!boxRef || !containerRef) return;
-
-    const box = boxRef.current;
-    const container = containerRef.current;
-
-    function onMouseDown(e: MouseEvent) {
-      isClicked.current = true;
-      coords.current.startX = e.clientX;
-      coords.current.startY = e.clientY;
-    }
-    function onMouseUp(e: MouseEvent) {
-      isClicked.current = false;
-
-      if (box) {
-        coords.current.lastX = box.offsetLeft;
-        coords.current.lastY = box.offsetTop;
-      }
-    }
-    function onMouseMove(e: MouseEvent) {
-      if (!isClicked.current) return;
-
-      const nextX = e.clientX - coords.current.startX + coords.current.lastX;
-      const nextY = e.clientY - coords.current.startY + coords.current.lastY;
-
-      if (box) {
-        box.style.left = `${nextX}px`;
-        box.style.top = `${nextY}px`;
-      }
-    }
-
-    box?.addEventListener("mousedown", onMouseDown);
-    box?.addEventListener("mouseup", onMouseUp);
-    container?.addEventListener("mousemove", onMouseMove);
-    container?.addEventListener("mouseleave", onMouseUp);
-
-    function cleanUp() {
-      box?.removeEventListener("mousedown", onMouseDown);
-      box?.removeEventListener("mouseup", onMouseUp);
-      container?.removeEventListener("mousemove", onMouseMove);
-      container?.removeEventListener("mouseleave", onMouseUp);
-    }
-
-    return cleanUp;
-  }, []);
-
   return (
-    // Container Box
-    <div ref={containerRef} className="h-full w-full bg-neutral-500">
-      {/* Test Box */}
-      <div
-        ref={boxRef}
-        className="absolute h-14 w-14 cursor-grab bg-black"
-      ></div>
+    <div className="h-full w-full bg-neutral-500">
+      <Draggable bounds="parent">
+        <div className="absolute h-14 w-14 cursor-grab bg-black"></div>
+      </Draggable>
     </div>
   );
 }
