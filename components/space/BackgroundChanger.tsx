45fe056b343052937395410db408e96dc41c27f2
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-05-27 21:56:38 +0530

    feat: Refactor data fetching and add cleanup

--- components/space/BackgroundChanger.tsx
+++ components/space/BackgroundChanger.tsx
@@ -1,38 +1,63 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { backgrounds } from "@/types/types";
 
-async function getBackgrounds() {
-  const apiKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
-  const controller = new AbortController();
-  const signal = controller.signal;
-  try {
-    const res = await fetch(
-      `https://api.unsplash.com/photos/?client_id=${apiKey}`,
-      { signal },
-    );
-
-    if (!res.ok) {
-      throw new Error("Something went wrong!");
-    }
-
-    const backgrounds = res.json();
-    console.log(backgrounds);
-  } catch (error) {
-    console.error("Error fetching images from Unsplash: " + error);
-  }
-
-  return () => {
-    controller.abort();
-  };
-}
+const apiKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
 
 function BackgroundChanger() {
-  useEffect(() => {
-    getBackgrounds();
+  const [backgrounds, setBackgrounds] = useState<backgrounds[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    return;
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchData = async () => {
+      try {
+        const res = await fetch(
+          `https://api.unsplash.com/photos/?client_id=${apiKey}`,
+          { signal },
+        );
+
+        if (!res.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const backgrounds = await res.json();
+        console.log(backgrounds);
+        setBackgrounds(backgrounds);
+        setLoading(false);
+      } catch (error) {
+        if (error instanceof Error) {
+          if (error.name === "AbortError") {
+            console.log("Fetch aborted");
+          } else {
+            setError(error.message);
+          }
+        } else {
+          setError("An unknown error occurred");
+        }
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
   }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
     <div className="absolute z-40 mb-28 flex h-44 w-[28rem] rounded-xl bg-neutral-50 shadow-lg"></div>
   );
--- types/types.ts
+++ types/types.ts
@@ -5,3 +5,11 @@ export type navbarItem = {
   href: string;
   icon: IconType;
 };
+
+export type backgrounds = {
+  id: string;
+  blur_hash: string;
+  color: string;
+  height: number;
+  width: number;
+};
