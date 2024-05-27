d02c230b6d33525dbe6e83502cf223b9612ed07f
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-05-27 22:25:38 +0530

    feat: Add tooltips

--- components/space/Navbar.tsx
+++ components/space/Navbar.tsx
@@ -3,6 +3,12 @@ import React, { useState } from "react";
 import { AiOutlinePicture as Picture } from "react-icons/ai";
 import { MdOutlineLibraryMusic as Music } from "react-icons/md";
 import BackgroundChanger from "./BackgroundChanger";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const Navbar = () => {
   const [activeItem, setActiveItem] = useState<number | null>(null);
@@ -18,46 +24,73 @@ const Navbar = () => {
       {openChanger && <BackgroundChanger />}
       <nav className="absolute z-40 mb-[3rem] flex items-center">
         <div className="mr-2 rounded-xl bg-neutral-50 px-1 py-1">
-          <li
-            key={7}
-            id="background-changer"
-            className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-100 ease-in-out  hover:bg-emerald-100 ${activeItem === 7 ? "bg-emerald-200 hover:bg-emerald-200" : ""} `}
-            onClick={() => {
-              handleClick(7);
-            }}
-          >
-            <span>{<Picture size={"1.35rem"} color="#262626" />}</span>
-          </li>
+          <TooltipProvider delayDuration={75} skipDelayDuration={75}>
+            <Tooltip>
+              <TooltipTrigger>
+                <li
+                  key={7}
+                  id="background-changer"
+                  className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-100 ease-in-out  hover:bg-emerald-100 ${activeItem === 7 ? "bg-emerald-200 hover:bg-emerald-200" : ""} `}
+                  onClick={() => {
+                    handleClick(7);
+                  }}
+                >
+                  <span>{<Picture size={"1.35rem"} color="#262626" />}</span>
+                </li>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Background</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
         <div className="w-[20rem] items-center justify-center rounded-xl border-0 bg-neutral-50 px-1 py-1 shadow-lg">
           <div className="flex flex-1 justify-between">
-            {navbarItems.map(({ id, icon: Icon }) => {
+            {navbarItems.map(({ id, icon: Icon, title }) => {
               const isActive = id === activeItem;
               return (
-                <li
-                  key={id}
-                  className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-200 ease-in-out hover:bg-emerald-100 ${isActive ? " rounded-lg bg-emerald-200 hover:bg-emerald-200" : ""}`}
-                  onClick={() => {
-                    handleClick(id);
-                  }}
-                >
-                  <span>{<Icon size={"1.35rem"} color="#262626" />}</span>
-                </li>
+                <TooltipProvider delayDuration={75} skipDelayDuration={75}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <li
+                        key={id}
+                        className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-200 ease-in-out hover:bg-emerald-100 ${isActive ? " rounded-lg bg-emerald-200 hover:bg-emerald-200" : ""}`}
+                        onClick={() => {
+                          handleClick(id);
+                        }}
+                      >
+                        <span>{<Icon size={"1.35rem"} color="#262626" />}</span>
+                      </li>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{title}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
               );
             })}
           </div>
         </div>
         <div className="ml-2 rounded-xl bg-neutral-50 px-1 py-1">
-          <li
-            id="music-player"
-            key={8}
-            className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-100 ease-in-out hover:bg-emerald-100 ${activeItem === 8 ? "bg-emerald-200 hover:bg-emerald-200" : ""} `}
-            onClick={() => {
-              handleClick(8);
-            }}
-          >
-            <span>{<Music size={"1.35rem"} color="#262626" />}</span>
-          </li>
+          <TooltipProvider delayDuration={75} skipDelayDuration={75}>
+            <Tooltip>
+              <TooltipTrigger>
+                <li
+                  id="music-player"
+                  key={8}
+                  className={`flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg transition-colors duration-100 ease-in-out hover:bg-emerald-100 ${activeItem === 8 ? "bg-emerald-200 hover:bg-emerald-200" : ""} `}
+                  onClick={() => {
+                    handleClick(8);
+                  }}
+                >
+                  <span>{<Music size={"1.35rem"} color="#262626" />}</span>
+                </li>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Music</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </nav>
     </>
