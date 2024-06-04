009098555cf588c2283d976d8f92028e39d8c029
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-06-04 23:14:26 +0530

    ui: Change color scheme for notes widget

--- components/space/Notes.tsx
+++ components/space/Notes.tsx
@@ -14,9 +14,9 @@ export default function Notes({}: Props) {
 
   return (
     <div className="absolute left-56 top-20 flex h-[30rem] min-w-[192px] rounded-lg bg-white shadow-lg">
-      <aside className="min-w-[14.5rem] overflow-auto rounded-l-lg border-r border-r-neutral-200">
+      <aside className="min-w-[14.5rem] overflow-auto rounded-l-lg border-r border-r-neutral-200 bg-[#F7F7F7]">
         <div>
-          <div className="sticky top-0 bg-white px-3 pb-2 pt-3">
+          <div className="sticky top-0 bg-[#F7F7F7] px-3 pb-2 pt-3">
             {/* Sidebar */}
             <span>
               <SidebarIcon
@@ -28,25 +28,25 @@ export default function Notes({}: Props) {
             </span>
             {/* Search Box */}
             <div className="mb-2 flex items-center gap-2">
-              <div className="flex items-center rounded-md border-2 border-neutral-400 pl-1">
+              <div className="flex items-center rounded-md border-2 border-[#E8E8E8] pl-1">
                 <Search size={"20px"} className="mr-1 fill-neutral-500" />
                 <input
                   type="text"
                   placeholder="Search"
-                  className="w-[144px] bg-transparent text-lg text-neutral-700 outline-none placeholder:text-neutral-400 focus:placeholder:text-transparent active:border-0"
+                  className="w-[144px] bg-transparent p-1 text-neutral-700 outline-none placeholder:text-neutral-400 focus:placeholder:text-transparent active:border-0"
                 />
               </div>
-              <div className="h-full w-full flex-1 flex-grow cursor-pointer rounded-md bg-neutral-500 p-2 transition-colors duration-100 hover:bg-neutral-700">
-                <NewNote size={"16px"} color="#fff" />
+              <div className="h-full w-full flex-1 flex-grow cursor-pointer rounded-md bg-[#8F8F8F] p-2 transition-colors duration-100 hover:bg-neutral-700">
+                <NewNote size={"19px"} color="#fff" />
               </div>
             </div>
           </div>
           {/* Notes */}
-          <div className="mt-1 flex flex-col gap-2 px-3 pb-2">
-            {Array.from({ length: 10 }).map((_, index) => (
+          <div className="flex flex-col gap-2 px-3 pb-2">
+            {Array.from({ length: 7 }).map((_, index) => (
               <div
                 key={index}
-                className="w-full cursor-pointer list-none rounded-lg bg-neutral-600 p-2 text-white transition-colors duration-100 hover:bg-neutral-700"
+                className="w-full cursor-pointer list-none rounded-lg bg-white p-2 transition-colors duration-100 hover:bg-neutral-200"
               >
                 {index}
               </div>
