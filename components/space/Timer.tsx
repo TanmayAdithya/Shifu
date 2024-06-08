4c854e174accd8385315f047d3160f4491165684
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-06-08 11:34:46 +0530

    feat: Setup basic working timer

--- components/space/Timer.tsx
+++ components/space/Timer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useTimer } from "react-timer-hook";
+import { FaPlay as Start } from "react-icons/fa6";
+import { FaPause as Pause } from "react-icons/fa6";
+import { VscDebugRestart as Restart } from "react-icons/vsc";
 
 type Props = {
   expiryTimestamp: Date;
@@ -14,43 +17,45 @@ function MyTimer({ expiryTimestamp }: Props) {
     });
 
   return (
-    <div className="flex flex-col">
-      <h3>Timer</h3>
-      <div>
+    <div className="flex w-full flex-col justify-between">
+      <div className="flex w-full">
+        <div className="flex flex-1 flex-grow cursor-pointer items-center justify-center rounded-l-lg border-y border-l border-neutral-200 bg-neutral-50 px-3 py-2 transition-colors duration-100 hover:bg-neutral-100">
+          Timer
+        </div>
+        <div className="flex flex-1 flex-grow cursor-pointer items-center justify-center rounded-r-lg border border-neutral-200 bg-neutral-50 px-3 py-2 transition-colors duration-100 hover:bg-neutral-100">
+          Stopwatch
+        </div>
+      </div>
+      <div className="flex items-center justify-center text-5xl font-light">
         <span>{hours > 0 && (hours < 10 ? "0" + hours : hours)}</span>:
         <span>{minutes < 10 ? "0" + minutes : minutes}</span>:
         <span>{seconds < 10 ? "0" + seconds : seconds}</span>
       </div>
-      <p>{isRunning ? "Running" : "Not running"}</p>
-      <div className="flex flex-wrap gap-3">
-        <button
-          className="mr-2 rounded-full bg-blue-400 px-3 py-1 text-emerald-50 transition-colors duration-100 hover:bg-blue-600"
+      {/* <p>{isRunning ? "Running" : "Not running"}</p> */}
+      <div className="flex flex-wrap justify-center gap-3">
+        <span
+          className="mr-2 flex cursor-pointer items-center justify-center rounded-full bg-neutral-600 p-3 text-emerald-50 transition-colors duration-100 hover:bg-neutral-800"
           onClick={start}
         >
-          Start
-        </button>
-        <button
-          className="mr-2 rounded-full bg-blue-400 px-3 py-1 text-emerald-50 transition-colors duration-100 hover:bg-blue-600"
+          <Start size={"20px"} />
+        </span>
+        <span
+          className="mr-2 flex cursor-pointer items-center justify-center rounded-full bg-neutral-600 p-3 text-emerald-50 transition-colors duration-100 hover:bg-neutral-800"
           onClick={pause}
         >
-          Pause
-        </button>
-        <button
-          className="mr-2 rounded-full bg-blue-400 px-3 py-1 text-emerald-50 transition-colors duration-100 hover:bg-blue-600"
-          onClick={resume}
-        >
-          Resume
-        </button>
-        <button
-          className="mr-2 rounded-full bg-blue-400 px-3 py-1 text-emerald-50 transition-colors duration-100 hover:bg-blue-600"
+          <Pause size={"20px"} />
+        </span>
+
+        <span
+          className="mr-2 flex cursor-pointer items-center justify-center rounded-full bg-neutral-600 p-3 text-emerald-50 transition-colors duration-100 hover:bg-neutral-800"
           onClick={() => {
             const time = new Date();
             time.setSeconds(time.getSeconds() + 300);
             restart(time);
           }}
         >
-          Restart
-        </button>
+          <Restart size={"20px"} />
+        </span>
       </div>
     </div>
   );
