b486a9de8379d18dc6f1b25c0c4d21d8a7d64ae8
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-07-11 17:19:09 +0530

    refactor/timer: Redesign UI to resemble a real timer

--- components/space/Timer.tsx
+++ components/space/Timer.tsx
@@ -1,65 +1,55 @@
-import React, { useState } from "react";
+import React from "react";
 import MinimizeWidget from "./MinimizeWidget";
-import { TimerProps } from "@/types/types";
-import { FaPlus as Add } from "react-icons/fa";
-import { FaMinus as Minus } from "react-icons/fa";
+import { PiPlayPauseFill as PlayPause } from "react-icons/pi";
+import { GrPowerReset as Reset } from "react-icons/gr";
+import { GoStopwatch as Stopwatch } from "react-icons/go";
 
 type Props = { openTimerWidget: boolean };
 
-// start/stop
-//pause
-//reset
-//mode
-//
-
 const Timer = ({ openTimerWidget }: Props) => {
-  const [time, setTime] = useState<TimerProps>({
-    seconds: 0,
-    minutes: 0,
-    hours: 0,
-  });
-
-  function startTimer() {}
-
   return (
     <div
       className={`${openTimerWidget ? "" : "hidden"} absolute bottom-80 right-[35rem] aspect-square w-72 rounded-3xl bg-white p-7`}
     >
-      <div className="mb-4 rounded-xl border border-neutral-300 bg-neutral-200/60 text-center">
-        <span className="pointer-events-none text-8xl tracking-tighter text-neutral-700">
+      <div className="relative mb-6 rounded-xl border border-neutral-300 bg-neutral-200/60 text-center">
+        <div className="pointer-events-none absolute left-[60%] top-1 select-none text-xs text-neutral-500">
+          M
+        </div>
+        <div className="pointer-events-none absolute bottom-10 right-[10%] select-none text-xs text-neutral-500">
+          S
+        </div>
+        <span className="pointer-events-none select-none text-8xl tracking-tighter text-neutral-700">
           00
         </span>
-        <span className="pointer-events-none text-5xl tracking-tighter text-neutral-700">
+        <span className="pointer-events-none select-none text-5xl tracking-tighter text-neutral-700">
           00
         </span>
       </div>
 
-      <div className="flex items-center justify-between">
-        <div>
-          <h3 className="text-center text-lg text-neutral-700">MIN</h3>
-          <div>
-            <button className="mr-1 rounded-s-full bg-neutral-500 p-2 text-neutral-200 transition-colors duration-300 hover:bg-neutral-600 hover:text-neutral-100">
-              <Add size={"10px"} className="text-neutral-100" />
-            </button>
-            <button className="rounded-e-full bg-neutral-500 p-2 text-neutral-200 transition-colors duration-300 hover:bg-neutral-600 hover:text-neutral-100">
-              <Minus size={"10px"} className="text-neutral-100" />
-            </button>
-          </div>
-        </div>
-        <div>
-          <h3 className="text-center text-lg text-neutral-700">SEC</h3>
-          <div>
-            <button className="mr-1 rounded-s-full bg-neutral-500 p-2 text-neutral-200 transition-colors duration-300 hover:bg-neutral-600 hover:text-neutral-100">
-              <Add size={"10px"} className="text-neutral-100" />
-            </button>
-            <button className="rounded-e-full bg-neutral-500 p-2 text-neutral-200 transition-colors duration-300 hover:bg-neutral-600 hover:text-neutral-100">
-              <Minus size={"10px"} className="text-neutral-100" />
-            </button>
-          </div>
-        </div>
+      <div className="mb-4 flex items-center justify-between">
+        <button className="flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-neutral-700 text-neutral-800 transition-colors duration-300 hover:bg-neutral-700 hover:text-neutral-100">
+          10M
+        </button>
+        <button className="flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-neutral-700 text-neutral-800 transition-colors duration-300 hover:bg-neutral-700 hover:text-neutral-100">
+          5M
+        </button>
+        <button className="flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-neutral-700 text-neutral-800 transition-colors duration-300 hover:bg-neutral-700 hover:text-neutral-100">
+          1M
+        </button>
+        <button className="flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-neutral-700 text-neutral-800 transition-colors duration-300 hover:bg-neutral-700 hover:text-neutral-100">
+          1S
+        </button>
       </div>
-      <div className="flex">
-        <button></button>
+      <div className="flex items-center justify-between">
+        <button className="flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-neutral-700 text-sm text-neutral-800 transition-colors duration-300 hover:bg-neutral-700 hover:text-neutral-100">
+          <Stopwatch size={"20px"} />
+        </button>
+        <button className="flex h-12 w-12 items-center justify-center rounded-full border-[1.5px] border-neutral-700 text-neutral-800 transition-colors duration-300 hover:bg-neutral-700 hover:text-neutral-100">
+          <Reset size={"20px"} />
+        </button>
+        <button className="flex h-12 w-28 items-center justify-center rounded-full border-[1.5px] border-neutral-700 bg-neutral-700 text-white transition-colors duration-300 hover:bg-neutral-900">
+          <PlayPause size={"30px"} />
+        </button>
       </div>
 
       <div className={`absolute right-3 top-2`}>
