d85006554c9d4c8611687701e22b8ec3f669a617
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-09-17 21:57:56 +0530

    fix(notes): force notes route to be dynamic

--- app/api/notes/route.ts
+++ app/api/notes/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { NotesCollection } from "@/lib/db";
 import { validateRequest } from "@/lib/auth";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     const { user } = await validateRequest();
