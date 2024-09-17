e453f5079f7808b99a64eb6466fdbb3df7efe83e
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-09-17 21:51:58 +0530

    feat: Add tabs, axios, and youtube route handler

---
+++ app/api/youtube/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse, NextRequest } from "next/server";
+import axios from "axios";
+
+export async function GET(req: NextRequest) {
+  const API_KEY = process.env.YOUTUBE_API_KEY;
+
+  if (!API_KEY) {
+    throw new Error("Something went wrong with the .env");
+  }
+
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get("q");
+
+  const BASE_URL = `https://youtube.googleapis.com/youtube/v3/search`;
+
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: query,
+        part: "snippet",
+        key: API_KEY,
+        maxResults: "10",
+        type: "video",
+      },
+    });
+
+    return NextResponse.json(response.data, { status: 200 });
+  } catch (error) {
+    console.error("YouTube API error: ", error);
+    return NextResponse.json(
+      { message: "Failed to fetch videos" },
+      { status: 500 },
+    );
+  }
+}
