04fff8cc76b90cd93711964f789a33499d2eda86
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-10-04 12:46:38 +0530

    feat: prevent multiple Redis connections

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,17 +1,37 @@
-import { createClient } from "redis";
+import { createClient, RedisClientType } from "redis";
 
-const redis = createClient({
-  password: process.env.REDIS_PASSWORD,
-  socket: {
-    host: process.env.REDIS_HOST_URL,
-    port: 15803,
-  },
+let redis: RedisClientType;
+
+const initRedis = async () => {
+  if (!redis || !redis.isOpen) {
+    redis = createClient({
+      password: process.env.REDIS_PASSWORD,
+      socket: {
+        host: process.env.REDIS_HOST_URL,
+        port: 15803,
+      },
+    });
+
+    redis.on("error", (err) => console.error("Redis client error: ", err));
+
+    await redis.connect();
+    console.log("Redis connected");
+  } else {
+    console.log("Using existing Redis connection");
+  }
+};
+
+await initRedis();
+
+process.on("SIGINT", async () => {
+  try {
+    if (redis) {
+      await redis.disconnect();
+    }
+  } catch (error) {
+    console.error("Failed to close Redis connection:", error);
+  }
+  process.exit(0);
 });
-
-redis.on("error", (err) => console.log("Redis client error: ", err));
-
-if (!redis.isOpen) {
-  redis.connect();
-}
 
 export { redis };
